b4d210407374f4ff0203d43e0358668b48a221e3
MatthewWeigand99 <mattweigand99@gmail.com>
2023-10-21 17:50:33 -0400

    Created basic game class

---
+++ game.py
@@ -0,0 +1,25 @@
+import sys
+import pygame
+
+class Game:
+    def __init__(self):  
+        pygame.init()
+
+        SCREEN_WIDTH = 800
+        SCREEN_HEIGHT = 600
+
+        pygame.display.set_caption('Ninja Platformer')
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            pygame.display.update()
+            self.clock.tick(60) # 60 FPS loop
+            
+Game().run()
