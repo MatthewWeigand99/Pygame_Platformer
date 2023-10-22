23a29622e1b4af648f2cae7d6c37cc097860d4cd
MatthewWeigand99 <mattweigand99@gmail.com>
2023-10-21 20:11:56 -0400

    Created scripts folder and added PhysicsEntity class

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+from scripts.entities import PhysicsEntity
+
 class Game:
     def __init__(self):  
         pygame.init()
@@ -12,12 +14,33 @@ class Game:
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pygame.time.Clock()
 
+        self.movement = [False, False]
+
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+        
     def run(self):
         while True:
+            self.screen.fill((14, 219, 248)) # Background color
+            
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.screen)
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w or event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_s or event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                        
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_w or event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_s or event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
             
             pygame.display.update()
             self.clock.tick(60) # 60 FPS loop
