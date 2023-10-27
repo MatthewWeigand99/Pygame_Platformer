ba2798a25111b01928c1bbaaa722f8819c430ace
MatthewWeigand99 <mattweigand99@gmail.com>
2023-10-26 20:49:02 -0400

    Created tilemap file and added basic collision and jumping physics

--- game.py
+++ game.py
@@ -2,51 +2,65 @@ import sys
 import pygame
 
 from scripts.entities import PhysicsEntity
-from scripts.utils import load_image
+from scripts.utils import load_image, load_images
+from scripts.tilemap import Tilemap
 
 class Game:
     def __init__(self):  
         pygame.init()
 
-        SCREEN_WIDTH = 800
-        SCREEN_HEIGHT = 600
+        SCREEN_WIDTH = 640
+        SCREEN_HEIGHT = 480
 
         pygame.display.set_caption('Ninja Platformer')
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.clock = pygame.time.Clock()
+        self.display = pygame.Surface((320, 240))
 
+        self.clock = pygame.time.Clock()
+    
         self.movement = [False, False]
         
         self.assests = {
+            'decor' : load_images('tiles/decor'),
+            'grass' : load_images('tiles/grass'),
+            'large_decor' : load_images('tiles/large_decor'),
+            'stone' : load_images('tiles/stone'),
             'player' : load_image('entities/player.png')
         }
 
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
         
+        self.tilemap = Tilemap(self, tile_size=16)
+        
     def run(self):
         while True:
-            self.screen.fill((14, 219, 248)) # Background color
+            self.display.fill((14, 219, 248)) # Background color
             
-            self.player.update((self.movement[1] - self.movement[0], 0))
-            self.player.render(self.screen)
+            self.tilemap.render(self.display)
             
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w or event.key == pygame.K_LEFT:
+                    if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                         self.movement[0] = True
-                    if event.key == pygame.K_s or event.key == pygame.K_RIGHT:
+                    if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                         self.movement[1] = True
-                        
+                    if event.key == pygame.K_w or event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
+                      
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_w or event.key == pygame.K_LEFT:
+                    if event.key == pygame.K_a or event.key == pygame.K_LEFT:
                         self.movement[0] = False
-                    if event.key == pygame.K_s or event.key == pygame.K_RIGHT:
+                    if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                         self.movement[1] = False
             
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
             self.clock.tick(60) # 60 FPS loop
             
